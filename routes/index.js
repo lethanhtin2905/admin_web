bc196643d413f9e1d42eeddeba8a2f7f11220e0a
= <=>
2020-12-04 17:18:32 +0700

    add product and edit product

--- routes/index.js
+++ routes/index.js
@@ -3,14 +3,16 @@ var router = express.Router();
 const passport = require('passport');
 
 var homeControllers = require('../controller/home-controllers');
-
+var productControllers = require('../controller/product-controllers');
 const upload = require('../uploadMiddleware');
 
 
 /* GET home page. */
 router.get('/', homeControllers.index);
-
 router.get('/home', homeControllers.index);
 
+// Product
+router.get('/product', productControllers.displayProducts);
+router.post('/product/add',upload.single('image'), productControllers.addProduct);
 
 module.exports = router;
