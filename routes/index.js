15ddf82e5932063c0630d75548b2487b9346d777
trantuantronglqd <=>
2020-12-08 18:22:56 +0700

    fix "upload image" feature, paging all product

--- routes/index.js
+++ routes/index.js
@@ -4,7 +4,6 @@ const passport = require('passport');
 
 var homeControllers = require('../controller/home-controllers');
 var productControllers = require('../controller/product-controllers');
-var storeControllers = require('../controller/store-controllers');
 const upload = require('../uploadMiddleware');
 
 
@@ -17,10 +16,5 @@ router.get('/product', productControllers.displayProducts);
 router.post('/product/add',upload.single('image'), productControllers.addProduct);
 router.post('/product/edit',upload.single('image'), productControllers.editProduct);
 router.get('/product/delete/:id', productControllers.deleteProduct);
-
-// Store
-router.get('/store', storeControllers.displayStore);
-router.post('/store/add',upload.single('image'), storeControllers.addStore);
-router.get('/store/delete/:id', storeControllers.deleteStore);
 
 module.exports = router;
