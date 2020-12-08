b23228e3478e65ed27f1627e84520482c4ea1bff
THỨC TRIỆU MAI NGỌC <18120585@student.hcmus.edu.vn>
2020-12-08 18:50:05 +0700

    edit header, datatables.js

--- app.js
+++ app.js
@@ -41,7 +41,6 @@ app.use(bodyParser.urlencoded({extended: true}));
 // Static path
 app.use(express.static(path.join(__dirname, 'public')));
 app.use('/category', express.static(path.join(__dirname, 'public')));
-app.use('/store', express.static(path.join(__dirname, 'public')));
 app.use('/product', express.static(path.join(__dirname, 'public')));
 
 // Express Session
